1220986a4176dad683b61b562e0d02b23e6b1593
LeBW <27091925@qq.com>
2018-06-29 11:46:25 +0800

    add run loop

--- nju_eas.py
+++ nju_eas.py
@@ -20,7 +20,10 @@ class NJUEas:
         self.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) ' \
                           'Chrome/63.0.3239.132 Safari/537.36 '
         # 0: 通识补选 1: 公选补选 2: 体育补选 3: 阅读补选 4: 专业选课
-        self.course_types = ['submitDiscussRenew', 'submitPublicRenew', 'addGymSelect', 'readRenewCourseSelect', 'addSpecialitySelect']
+        self.course_types = ['submitDiscussRenew', 'submitPublicRenew', 'addGymSelect', 'readRenewCourseSelect',
+                             'addSpecialitySelect']
+
+        self.is_login = False
 
     def __get_val_code(self):
         val_code = self.s.get(self.code_url)
@@ -28,6 +31,10 @@ class NJUEas:
             f.write(val_code.content)
 
     def log_in(self):
+        if self.is_login:
+            print('已登陆')
+            return
+
         config = configparser.ConfigParser()
         config.read('nju_eas.ini')
         try:
@@ -40,9 +47,18 @@ class NJUEas:
         code = input('请输入验证码: ')
         data = {'returnUrl': 'null', 'userName': username, "password": password, 'ValidateCode': code}
         # log in
-        r = self.s.post(self.login_url, data)
+        try:
+            r = self.s.post(self.login_url, data)
+        except requests.exceptions.RequestException as e:
+            print('Request error:', e)
+            return
         soup = BeautifulSoup(r.text, 'lxml')
-        print(soup.find(id='UserInfo').text)
+
+        try:
+            print(soup.find(id='UserInfo').text)
+            self.is_login = True
+        except AttributeError:
+            print(soup.find(id='Main').text)
 
     def grab_course(self):
         while True:
@@ -71,11 +87,13 @@ class NJUEas:
         r = requests.Response()
         try:
             if course_type == 0 or course_type == 1:
-                r = self.s.get(self.renew_course_url, params={'method': method, 'classId': course_number, 'campus': '仙林校区'})
+                r = self.s.get(self.renew_course_url,
+                               params={'method': method, 'classId': course_number, 'campus': '仙林校区'})
             elif course_type == 2:
                 r = self.s.post(self.select_course_url, data={'method': method, 'classId': course_number})
             elif course_type == 3:
-                r = self.s.post(self.renew_course_url, data={'method': method, 'classId': course_number, 'type': 5, '_': ''})
+                r = self.s.post(self.renew_course_url,
+                                data={'method': method, 'classId': course_number, 'type': 5, '_': ''})
             elif course_type == 4:
                 r = self.s.post(self.select_course_url, data={'method': method, 'classId': course_number})
             while '已满' in r.text:
@@ -83,11 +101,13 @@ class NJUEas:
                 count += 1
                 time.sleep(2)
                 if course_type == 0 or course_type == 1:
-                    r = self.s.get(self.renew_course_url, params={'method': method, 'classId': course_number, 'campus': '仙林校区'})
+                    r = self.s.get(self.renew_course_url,
+                                   params={'method': method, 'classId': course_number, 'campus': '仙林校区'})
                 elif course_type == 2:
                     r = self.s.post(self.select_course_url, data={'method': method, 'classId': course_number})
                 elif course_type == 3:
-                    r = self.s.post(self.renew_course_url, data={'method': method, 'classId': course_number, 'type': 5, '_': ''})
+                    r = self.s.post(self.renew_course_url,
+                                    data={'method': method, 'classId': course_number, 'type': 5, '_': ''})
                 elif course_type == 4:
                     r = self.s.post(self.select_course_url, data={'method': method, 'classId': course_number})
         except requests.exceptions.RequestException as e:
@@ -95,12 +115,29 @@ class NJUEas:
         finally:
             print(r.text)
 
+    @staticmethod
+    def print_menu():
+        print('********0: 登陆********')
+        print('********1: 抢课********')
+        print('********q: 退出********')
+
+    def start(self):
+        print('欢迎进入南大教务系统小助手')
+        self.print_menu()
+        i = input()
+        while i != 'q':
+            if i == '0':
+                self.log_in()
+            elif i == '1':
+                self.grab_course()
+            self.print_menu()
+            i = input()
+        print('Bye~')
+
 
 def main():
     eas = NJUEas()
-    eas.log_in()
-    # test to grab some class.
-    eas.grab_course()
+    eas.start()
 
 
 if __name__ == '__main__':
